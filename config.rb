98b5318c5912755c8db1a48a0b6b6a1cb41b19a9
Paul Wilson <paul@vermonster.com>
2015-02-15 23:21:56 -0500

    Create separate pages for each program

--- config.rb
+++ config.rb
@@ -18,10 +18,9 @@ activate :livereload
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+data.programs.programs.each do |program|
+  proxy "#{program.slug}.html", "/programs.html", :locals => { :program => program }, :ignore => true
+end
 
 ###
 # Helpers
